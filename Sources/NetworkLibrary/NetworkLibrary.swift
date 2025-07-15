5fc25f1894dce1232bc433f9a665d7defd6e0394
siddhant0706mob <2001siddhantranjan@gmail.com>
2025-07-15 21:17:19 +0530

    Initial Changes

--- Sources/NetworkLibrary/NetworkLibrary.swift
+++ Sources/NetworkLibrary/NetworkLibrary.swift
@@ -1,2 +1,24 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import Foundation
+
+public enum MethodType {
+    case GET
+    case POST
+    case PUT
+    case PATCH
+    case DELETE
+    case OPTIONS
+    case HEAD
+    case CONNECT
+    case TRACE
+}
+
+public protocol Request {
+    
+    var methodType: MethodType { get }
+    
+    var endpoint: String { get }
+    
+}
